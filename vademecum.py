56fe8d722be5514d076b5de192b281d21496e3d4
luis1415 <luisbermudeztorres@gmail.com>
2017-07-26 08:49:20 -0500

    Excel Mongo class

--- vademecum.py
+++ vademecum.py
@@ -3,15 +3,26 @@ from pymongo import MongoClient
 import pandas as pd
 import json
 
-client = MongoClient('localhost', 27017)
-# Se crea la base de datos
-db = client.vademecum
-db = client['vademecum']
 
-# Lee el excel
-analgesicos = pd.read_excel('analgesicos.xls').to_json(orient='records')
+class ExcelMongo:
+    def __init__(self, excel):
+        self.excel = excel
 
-# Se pasa a json y luego se inserta
-data = json.loads(analgesicos)
-vademecum = db.vademecum
-result = vademecum.insert(data)
+    # Lee el excel
+    def excel_to_mongo(self, excel):
+        analgesicos = pd.read_excel(excel).to_json(orient='records')
+        # Se pasa a json y luego se inserta
+        data = json.loads(analgesicos)
+        vademecum = db.vademecum
+        result = vademecum.insert(data)
+        return result
+
+if __name__ == '__main__':
+    # Conexion con la base de datos
+    client = MongoClient('localhost', 27017)
+    # Se crea la base de datos
+    db = client.vademecum
+    db = client['vademecum']
+    analgesico = ExcelMongo('analgesicos.xls')
+    analgesico.excel_to_mongo(analgesico.excel)
+
